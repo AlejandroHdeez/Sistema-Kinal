8d6bf314c93dbe172d3acad70fa3873cd5b7e99f
YoungFalla <andreefalla@gmail.com>
2019-04-25 10:30:05 -0600

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { FamiliaComponent } from './components/familia/familia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    FamiliaComponent
   ],
   imports: [
     BrowserModule,
